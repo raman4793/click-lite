ba12cb8ab4b39aa5cb3ffb0c550dc953ee6f2322
Ram <raman4793@gmail.com>
2023-06-23 12:10:34 +0100

    refactoring signature_reader module

---
+++ click_lite/signature_reader/__init__.py
@@ -0,0 +1,13 @@
+"""
+Contains classes to help with reading a signature of `callable` object.
+
+Has the following classes:
+
+- [SignatureReader][click_lite.signature_reader.SignatureReader]
+- [Signature][click_lite.signature_reader.Signature]
+- [Description][click_lite.signature_reader.Description]
+- [ParameterDescription][click_lite.signature_reader.ParameterDescription]
+- [Parameter][click_lite.signature_reader.Parameter]
+- [DocStringParser][click_lite.signature_reader.DocStringParser]
+
+"""
